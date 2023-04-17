e05566cb6b0147750a6a4acc0d53d1d1a52459c9
Amelia Risner <amelia.risner0@gmail.com>
2023-04-17 14:19:25 -0700

    config.properties file added, api keys hidden

--- server/src/main/java/swim/tutorial/DataSource.java
+++ server/src/main/java/swim/tutorial/DataSource.java
@@ -8,7 +8,9 @@ import com.twitter.clientlib.TwitterCredentialsBearer;
 import com.twitter.clientlib.ApiException;
 import com.twitter.clientlib.api.TwitterApi;
 import com.twitter.clientlib.model.*;
-
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 /**
  * Simple wrapper around some {@code SwimRef}, e.g. a {@code SwimClient} handle,
  * that pushes data to the Swim server running at {@code hostUri}.
@@ -53,43 +55,59 @@ class DataSource {
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     /**
      * Set the credentials for the required APIs.
      * The Java SDK supports TwitterCredentialsOAuth2 & TwitterCredentialsBearer.
      * Check the 'security' tag of the required APIs in https://api.twitter.com/2/openapi.json in order
      * to use the right credential object.
      */
-    TwitterApi apiInstance = new TwitterApi(new TwitterCredentialsBearer(System.getenv("AAAAAAAAAAAAAAAAAAAAAD26mQEAAAAAB4k8fymZdir8qK2uvh5xxwvIlwE%3DvN16pLFBzwUj1Gj1aAQK7HkmKRBGThXis33yMIVnUqEeoPuj4W")));
 
-    Set<String> tweetFields = new HashSet<>();
-    tweetFields.add("author_id");
-    tweetFields.add("id");
-    tweetFields.add("created_at");
+    Properties props = new Properties();
+    FileInputStream fis = new FileInputStream("config.properties");
+    props.load(fis);
+    fis.close();
 
-    try {
-     // findTweetById
-     Get2TweetsIdResponse result = apiInstance.tweets().findTweetById("1646217242651025410")
-      .tweetFields(tweetFields)
-      .execute();
-     if(result.getErrors() != null && result.getErrors().size() > 0) {
-       System.out.println("Error:");
+    // Retrieve the values
+    String apiKey = props.getProperty("api_key");
+    String apiSecretKey = props.getProperty("api_secret_key");
+    String bearerToken = props.getProperty("bearer_token");
 
-       result.getErrors().forEach(e -> {
-         System.out.println(e.toString());
-         if (e instanceof ResourceUnauthorizedProblem) {
-           System.out.println(((ResourceUnauthorizedProblem) e).getTitle() + " " + ((ResourceUnauthorizedProblem) e).getDetail());
-         }
-       });
-     } else {
-       System.out.println("findTweetById - Tweet Text: " + result.toString());
-     }
-    } catch (ApiException e) {
-      System.err.println("Status code: " + e.getCode());
-      System.err.println("Reason: " + e.getResponseBody());
-      System.err.println("Response headers: " + e.getResponseHeaders());
-      e.printStackTrace();
-    }
+    // Use the values in your code
+    System.out.println("API Key: " + apiKey);
+    System.out.println("API Secret Key: " + apiSecretKey);
+    System.out.println("Bearer Token: " + bearerToken);
+
+    // TwitterApi apiInstance = new TwitterApi(new TwitterCredentialsBearer(System.getenv("AAAAAAAAAAAAAAAAAAAAAD26mQEAAAAAB4k8fymZdir8qK2uvh5xxwvIlwE%3DvN16pLFBzwUj1Gj1aAQK7HkmKRBGThXis33yMIVnUqEeoPuj4W")));
+
+    // Set<String> tweetFields = new HashSet<>();
+    // tweetFields.add("author_id");
+    // tweetFields.add("id");
+    // tweetFields.add("created_at");
+
+    // try {
+    //  // findTweetById
+    //  Get2TweetsIdResponse result = apiInstance.tweets().findTweetById("1646217242651025410")
+    //   .tweetFields(tweetFields)
+    //   .execute();
+    //  if(result.getErrors() != null && result.getErrors().size() > 0) {
+    //    System.out.println("Error:");
+
+    //    result.getErrors().forEach(e -> {
+    //      System.out.println(e.toString());
+    //      if (e instanceof ResourceUnauthorizedProblem) {
+    //        System.out.println(((ResourceUnauthorizedProblem) e).getTitle() + " " + ((ResourceUnauthorizedProblem) e).getDetail());
+    //      }
+    //    });
+    //  } else {
+    //    System.out.println("findTweetById - Tweet Text: " + result.toString());
+    //  }
+    // } catch (ApiException e) {
+    //   System.err.println("Status code: " + e.getCode());
+    //   System.err.println("Reason: " + e.getResponseBody());
+    //   System.err.println("Response headers: " + e.getResponseHeaders());
+    //   e.printStackTrace();
+    // }
   }
 
 }
